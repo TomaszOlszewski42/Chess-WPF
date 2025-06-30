caa9d90bea9ecf485d626a32fa6fe4cc78b9a03e
Tomasz Olszewski <tomaszolszewski42@gmail.com>
2025-06-30 20:24:15 +0200

    pawn can capture now

--- ChessBoard/Movement/PawnMovement.cs
+++ ChessBoard/Movement/PawnMovement.cs
@@ -12,9 +12,17 @@ class PawnMovement : AbstractMovement
 	public override List<Tile> PossibleMoves(Tile[,] tiles, int start_x, int start_y)
 	{
 		List<Tile> result = new List<Tile>();
+		PlayerEnum owner = tiles[start_y, start_x].ChessPiece.Owner;
 
-		if (start_y + _direction < 8 && start_y + _direction >= 0 && tiles[start_y + _direction, start_x].ChessPiece == null)
+		if (start_y + _direction >= 8 || start_y + _direction < 0)
+			return result;
+
+		if (tiles[start_y + _direction, start_x].ChessPiece == null)
 			result.Add(tiles[start_y + _direction, start_x]);
+		if (start_x + 1 < 8 && tiles[start_y + _direction, start_x + 1].ChessPiece != null && tiles[start_y + _direction, start_x + 1].ChessPiece.Owner != owner)
+			result.Add(tiles[start_y + _direction, start_x + 1]);
+		if (start_x - 1 < 8 && tiles[start_y + _direction, start_x - 1].ChessPiece != null && tiles[start_y + _direction, start_x - 1].ChessPiece.Owner != owner)
+			result.Add(tiles[start_y + _direction, start_x - 1]);
 
 		return result;
 	}
